d475ffe5035726167b9e9d82188e255210963910
danielmej@unicauca.edu.co-computer science student <danielmej@unicauca.edu.co>
2023-10-21 18:23:43 -0500

    update dsl config

---
+++ dsl/createJob.groovy
@@ -0,0 +1,59 @@
+
+  folder("/${project["name"]}")
+
+  multibranchPipelineJob("/${project["name"]}/${repo["name"]}") {
+    description("${repo["name"]}")
+    branchSources {
+      branchSource {
+        source {
+          github {
+              id("${project["name"]}")
+              configuredByUrl(true)
+              repoOwner("aishroom")
+              repository("${repo["name"]}")
+              credentialsId('jenkins-bolivar')
+              repositoryUrl("https://github.com/aishroom/${repo["name"]}")
+              traits {
+                  gitHubBranchDiscovery {
+                      strategyId(3)
+                  }
+              }
+          }   
+        }
+        strategy {}
+      }
+    }
+    orphanedItemStrategy {
+      discardOldItems {}
+    }
+    factory {
+      remoteJenkinsFileWorkflowBranchProjectFactory {
+        remoteJenkinsFile("${repo["pipeline"]}")
+        localMarker('Jenkinsfile.json')
+        remoteJenkinsFileSCM {
+          gitSCM {
+            userRemoteConfigs    {
+              userRemoteConfig {
+                name('')
+                url('https://github.com/aishroom/pipelines')
+                refspec('')
+                credentialsId('jenkins-bolivar')
+              }
+            }
+            branches {
+              branchSpec {
+                name('master')
+              }
+            }
+            browser {} // required, but doesn't require configuration
+            gitTool('/usr/bin/env git') // or wherever makes sense
+          }
+        }
+        matchBranches(false)
+        fallbackBranch('master')
+        lookupInParameters(false)
+      }
+    }
+  }
+    
+  
